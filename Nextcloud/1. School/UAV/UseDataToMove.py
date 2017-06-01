43238b9e5d9fd6211bdae73bd24819ad6a8ba5be
Jeroen <jeroenkoot@outlook.com>
2017-06-01 15:07:03 +0200

    Update move-it en Thread voor afstandsmetingen

--- Nextcloud/1. School/UAV/UseDataToMove.py
+++ Nextcloud/1. School/UAV/UseDataToMove.py
@@ -12,7 +12,7 @@ def init():
     while uav.state.emergency_mask:
         print("emergency")
         uav.send(at.REF(0b0100000000))
-        time.sleep(0.1)
+        time.sleep(1)
     print("ready")
     return uav
 
@@ -31,9 +31,8 @@ def get_data(drone):                                    #get money, get bitches,
 def data_print(drone, previous_data):
     current_data = get_data(drone)
     if (previous_data!=current_data):
-        print("Altitude: ", altitude, "\t vx: ", vx, "\t vy: ", vy, "\t vz: ", vz, "\t phi: ", phi, "\t psi: ", psi,
-              "\t theta: ", theta)
-
+        print("Altitude: ", previous_data[0], "\t vx: ", previous_data[1], "\t vy: ", previous_data[2], "\t vz: ", previous_data[3], "\t phi: ", previous_data[4], "\t psi: ", previous_data[5],
+              "\t theta: ", previous_data[6])
     return(current_data)
 
 drone = init()
@@ -42,18 +41,92 @@ previous_data = get_data(drone)
 
 previous_data = data_print(drone, previous_data)
 
+speed = 0.1
+ActionTime = 2
+SleepTime = 1
+
 while not drone.state.fly_mask:
     drone.takeoff()
     print("Vlieg op!")
 
 print("JA baas")
 
-timeout = time.time() + 10
+drone.hover()
+print("hover")
+time.sleep(SleepTime*3)
+
+print("Omhoog")
+timeout = time.time() + ActionTime
 while True:
-    drone.hover()
+    drone.move(up=speed*4)
     previous_data = data_print(drone, previous_data)
     if time.time() > timeout:
-        print("hij is van de andere kant")
+        drone.move(up=0)
+        break
+
+drone.hover()
+print("hover")
+time.sleep(SleepTime)
+
+print("Vooruit met die geit")
+timeout = time.time() + ActionTime
+while True:
+    drone.move(forward=speed)
+    previous_data = data_print(drone, previous_data)
+    if time.time() > timeout:
+        drone.move(forward=0)
+        break
+
+drone.hover()
+print("hover")
+time.sleep(SleepTime)
+
+print("Rechts")
+timeout = time.time() + ActionTime
+while True:
+    drone.move(right=speed)
+    previous_data = data_print(drone, previous_data)
+    if time.time() > timeout:
+        drone.move(right=0)
+        break
+
+drone.hover()
+print("hover")
+time.sleep(SleepTime)
+
+print("en een stapje terug")
+timeout = time.time() + ActionTime
+while True:
+    drone.move(backward=speed)
+    previous_data = data_print(drone, previous_data)
+    if time.time() > timeout:
+        drone.move(backward=0)
+        break
+
+drone.hover()
+print("hover")
+time.sleep(SleepTime)
+
+print("Links")
+timeout = time.time() + ActionTime
+while True:
+    drone.move(left=speed)
+    previous_data = data_print(drone, previous_data)
+    if time.time() > timeout:
+        drone.move(left=0)
+        break
+
+drone.hover()
+print("hover")
+time.sleep(SleepTime)
+
+print("Omlaag")
+timeout = time.time() + ActionTime
+while True:
+    drone.move(down=speed*4)
+    previous_data = data_print(drone, previous_data)
+    if time.time() > timeout:
+        drone.move(down=0)
         break
 
 print("klaar")
