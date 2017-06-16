a18b0be9b15212e28928642817cc7be7f44e6336
Jeroen <jeroenkoot@outlook.com>
2017-06-16 14:29:12 +0200

    Camera checks

--- Nextcloud/1. School/UAV/video.py
+++ Nextcloud/1. School/UAV/video.py
@@ -6,8 +6,6 @@ from pyardrone import ARDrone, at
 def nothing(x):
     pass
 
-cv2.xfeatures2d.SIFT_create()
-
 
 def init():
     drone = ARDrone()
@@ -80,17 +78,17 @@ def filter_image(img, lower_mask, upper_mask):
 
     # Return binary image and slider data, so program remebers their position
     return thres, b, g, r, b1, g1, r1
-
-cam = cv2.VideoCapture(('tcp://192.168.1.1:5555'))
-
+print(" 1")
+cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
+print(" 2")
 init()
 lower_mask = np.array([0, 4, 148])
 upper_mask = np.array([255, 255, 255])
 i = 1
 ret = True
 while True:
-    img = cv2.imread("drone/img" + str(i) + ".jpg")
-    # ret, img = cam.read()
+    # img = cv2.imread("drone/img" + str(i) + ".jpg")
+    ret, img = cam.read()
     print(ret)
     if ret:
 
@@ -98,104 +96,106 @@ while True:
         lower_mask = [b, g, r]
         upper_mask = [b1, g1, r1]
         # cv2.imshow("thres", thres)
+        try:
+            im2, contours, hierarchy = cv2.findContours(thres, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        im2, contours, hierarchy = cv2.findContours(thres, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            hierarchy = hierarchy[0]
+            print("hierarchy: ", hierarchy)
 
-        hierarchy = hierarchy[0]
-        print("hierarchy: ", hierarchy)
+            marker = 0
+            MarkerInArea = True
+            area = np.array([[180, 0], [180, 360], [540, 360], [540, 0]])
+            cv2.drawContours(img, [area], 0, (255, 0, 0), 2)
 
-        marker = 0
-        MarkerInArea = True
-        area = np.array([[180, 0], [180, 360], [540, 360], [540, 0]])
-        cv2.drawContours(img, [area], 0, (255, 0, 0), 2)
+            for component in zip(contours, hierarchy):
+                currentContour = component[0]
+                currentHierarchy = component[1]
 
-        for component in zip(contours, hierarchy):
-            currentContour = component[0]
-            currentHierarchy = component[1]
+                if 100 < cv2.contourArea(currentContour) < 50000:
 
-            if 100 < cv2.contourArea(currentContour) < 50000:
+                    print("currentHierarchy: ", currentHierarchy)
+                    if currentHierarchy[2] >= 0 and currentHierarchy[3] >= 0:  # if contour has a child and a parent
+                        # currentContour should be black square, check if its parent doenst have a parent and its child
+                        # doesnt have a child
+                        rect = cv2.minAreaRect(currentContour)
+                        box = cv2.boxPoints(rect)
+                        box = np.int0(box)
 
-                print("currentHierarchy: ", currentHierarchy)
-                if currentHierarchy[2] >= 0 and currentHierarchy[3] >= 0:  # if contour has a child and a parent
-                    # currentContour should be black square, check if its parent doenst have a parent and its child
-                    # doesnt have a child
-                    rect = cv2.minAreaRect(currentContour)
-                    box = cv2.boxPoints(rect)
-                    box = np.int0(box)
+                        if hierarchy[currentHierarchy[2]][2] < 0 and hierarchy[currentHierarchy[3]][3] < 0:
+                            # Probably found a marker, yeey!
+                            MarkerContourOutside = contours[currentHierarchy[3]]
+                            MarkerContourInside = currentContour
+                            print("Contour ding: ", cv2.contourArea(MarkerContourInside)/cv2.contourArea(MarkerContourOutside))
+                            rect2 = cv2.minAreaRect(MarkerContourOutside)
+                            box2 = cv2.boxPoints(rect2)
+                            box2 = np.int0(box2)
+                            cv2.drawContours(img, [box2], 0, (0, 255, 255), 2)
+                            cv2.drawContours(img, [box], 0, (255, 255, 255), 2)
 
-                    if hierarchy[currentHierarchy[2]][2] < 0 and hierarchy[currentHierarchy[3]][3] < 0:
-                        # Probably found a marker, yeey!
-                        MarkerContourOutside = contours[currentHierarchy[3]]
-                        MarkerContourInside = currentContour
-                        print("Contour ding: ", cv2.contourArea(MarkerContourInside)/cv2.contourArea(MarkerContourOutside))
-                        rect2 = cv2.minAreaRect(MarkerContourOutside)
-                        box2 = cv2.boxPoints(rect2)
-                        box2 = np.int0(box2)
-                        cv2.drawContours(img, [box2], 0, (0, 255, 255), 2)
-                        cv2.drawContours(img, [box], 0, (255, 255, 255), 2)
-
-                        # Found and printed marker contours above. No check for circles in it.
-                        circleContour = contours[currentHierarchy[2]]
-                        circleHierarchy = hierarchy[currentHierarchy[2]]
-                        marker = 0
-                        breakNext = False
-
-                        if circleHierarchy[0] == -1:
-                            breakNext = True
-                            print("breakNext True")
-
-                        print("Hier ben ik")
-                        while True:
-                            print(circleHierarchy)
-                            rect3 = cv2.minAreaRect(circleContour)
-                            box3 = cv2.boxPoints(rect3)
-                            box3 = np.int0(box3)
-                            cv2.drawContours(img, [box3], 0, (255, 0, 0), 2)
-                            marker += 1
-                            circleHierarchy = hierarchy[circleHierarchy[0]]
-                            circleContour = contours[circleHierarchy[0]]
-
-                            if breakNext:
-                                print("gebroken")
-                                break
+                            # Found and printed marker contours above. No check for circles in it.
+                            circleContour = contours[currentHierarchy[2]]
+                            circleHierarchy = hierarchy[currentHierarchy[2]]
+                            marker = 0
+                            breakNext = False
 
                             if circleHierarchy[0] == -1:
                                 breakNext = True
                                 print("breakNext True")
 
+                            print("Hier ben ik")
+                            while True:
+                                print(circleHierarchy)
+                                rect3 = cv2.minAreaRect(circleContour)
+                                box3 = cv2.boxPoints(rect3)
+                                box3 = np.int0(box3)
+                                cv2.drawContours(img, [box3], 0, (255, 0, 0), 2)
+                                marker += 1
+                                circleHierarchy = hierarchy[circleHierarchy[0]]
+                                circleContour = contours[circleHierarchy[0]]
 
-                        moments = cv2.moments(MarkerContourOutside)
+                                if breakNext:
+                                    print("gebroken")
+                                    break
 
-                        cx = int(moments['m10'] / moments['m00'])
-                        cy = int(moments['m01'] / moments['m00'])
+                                if circleHierarchy[0] == -1:
+                                    breakNext = True
+                                    print("breakNext True")
 
-                        dx = cx - 320
-                        dy = cy - 180
-                        distanceToCenter = np.sqrt(dx * dx + dy * dy)
-                        cv2.line(img, (cx, cy), (320, 180), (255, 255, 255))
 
-                        # if distanceToCenter > 0:
-                        #     # move right
-                        #     cv2.putText(img, "Move: Right", (10, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255))
-                        # if distanceToCenter < 0:
-                        #     # move left
-                        #     cv2.putText(img, "Move: Left", (10, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255))
+                            moments = cv2.moments(MarkerContourOutside)
 
-        cv2.putText(img, str(marker), (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255))
-        print("marker = ", marker)
+                            cx = int(moments['m10'] / moments['m00'])
+                            cy = int(moments['m01'] / moments['m00'])
 
-        # for component in zip(contours, hierarchy):
-        #     currentContour = component[0]
-        #     currentHierarchy = component[1]
-        #     x, y, w, h = cv2.boundingRect(currentContour)
-        #     print(currentHierarchy[2])
-        #     if currentHierarchy[2] < 0:
-        #         # these are the innermost child components
-        #         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        #     elif currentHierarchy[3] < 0:
-        #         # these are the outermost parent components
-        #         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                            dx = cx - 320
+                            dy = cy - 180
+                            distanceToCenter = np.sqrt(dx * dx + dy * dy)
+                            cv2.line(img, (cx, cy), (320, 180), (255, 255, 255))
 
+                            # if distanceToCenter > 0:
+                            #     # move right
+                            #     cv2.putText(img, "Move: Right", (10, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255))
+                            # if distanceToCenter < 0:
+                            #     # move left
+                            #     cv2.putText(img, "Move: Left", (10, 80), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 255))
+
+            cv2.putText(img, str(marker), (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 255))
+            print("marker = ", marker)
+
+            # for component in zip(contours, hierarchy):
+            #     currentContour = component[0]
+            #     currentHierarchy = component[1]
+            #     x, y, w, h = cv2.boundingRect(currentContour)
+            #     print(currentHierarchy[2])
+            #     if currentHierarchy[2] < 0:
+            #         # these are the innermost child components
+            #         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            #     elif currentHierarchy[3] < 0:
+            #         # these are the outermost parent components
+            #         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        except TypeError:
+            print(" Ja Dat was me weer een errotje zeg")
         cv2.imshow("Image", img)
         cv2.waitKey(1)
         # time.sleep(3)
