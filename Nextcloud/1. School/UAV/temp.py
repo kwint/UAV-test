8aeacbb097ac08b3851c0c58a3459b67ec82ecde
Jeroen <jeroenkoot@outlook.com>
2017-06-19 19:28:15 +0200

    Camera checks

--- Nextcloud/1. School/UAV/temp.py
+++ Nextcloud/1. School/UAV/temp.py
@@ -1,27 +1,12 @@
-import cv2
 import time
-import numpy as np
-from pyardrone import ARDrone, at
-import threading
+import cv2
 
-print("import done")
+cam = cv2.VideoCapture(0)
+ret, img = cam.read()
 
-
-def droneMove():
-    print('hoi')
-
-
-
-
-moveData = 10
-drone = 1
-while True:
-    tijd = time.time()
-    movethread = threading.Thread(target=droneMove)
-    movethread.start()
-    print(time.time() - tijd)
-#
-# if not movethread.is_alive() and moveData.marker:
-#     movethread = threading.Thread(target=droneMove, args=(moveData, drone))
-#     movethread.start()
-#     moveData.marker = False
+tijd = time.ctime()
+print(tijd)
+tijd = tijd.replace(" ", "_")
+tijd = tijd.replace(":", "_")
+cv2.imwrite(tijd + ".jpg", img)
+print(tijd)
